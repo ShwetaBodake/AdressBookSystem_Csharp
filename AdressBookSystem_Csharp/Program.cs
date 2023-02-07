07c51702f85ad53436e313b22ef495ca7aa7c232
Shweta Bodake <shwetabodake8108@gmail.com>
2023-02-07 23:23:59 +0530

    [Shweta] Modified InCurrent adress book Added new Contact

--- AdressBookSystem_Csharp/Program.cs
+++ AdressBookSystem_Csharp/Program.cs
@@ -10,32 +10,42 @@ class Program
         {
             Console.WriteLine("-------------Welcome to Adress Book System---------------");
 
+            for (int i = 1; i <= 5; i++)
+            {
+                AddPerson();
+                Console.WriteLine("\n");
+                Console.WriteLine("Enter Contact:-");
+            }
+        }
+            private static void AddPerson()
+            {
+                ContactOfPerson person = new ContactOfPerson();
 
-        ContactOfPerson person = new ContactOfPerson();
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
 
-            Console.Write("Enter First Name: ");
-            person.FirstName = Console.ReadLine();
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
 
-            Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
+                Console.Write("Enter Phone Number: ");
 
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                person.PhoneNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter Address : ");
-            person.Adress = Console.ReadLine();
+                Console.Write("Enter Address : ");
+                person.Adress = Console.ReadLine();
 
-            Console.WriteLine("Enter City: ");
-            person.City = Console.ReadLine();
+                Console.WriteLine("Enter City: ");
+                person.City = Console.ReadLine();
 
-            Console.WriteLine("Enter State: ");
-            person.State = Console.ReadLine();
+                Console.WriteLine("Enter State: ");
+                person.State = Console.ReadLine();
 
-            Console.WriteLine("Enter Email: ");
-            person.Email = Console.ReadLine();
+                Console.WriteLine("Enter Email: ");
+                person.Email = Console.ReadLine();
 
-            People.Add(person);
+                People.Add(person);
+            }
         }
     }
-}
+
 
